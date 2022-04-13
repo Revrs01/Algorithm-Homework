4c43bb7847a44de7d9ef24b2fb1bc982eb9ea9e4
Phil Wu <philwu53@gmail.com>
2022-04-13 12:28:50 +0800

    finished optimal multiply time func

--- Matrix Chain Mul/matrixChainMultiplication.cpp
+++ Matrix Chain Mul/matrixChainMultiplication.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 int matrixAmount;
 vector<vector<int>> cutLoc;
-int mpy(int i,int j, vector<vector<int>> currentTimes){
 
+void matrixChainMul(vector<int> &p) {
+    int matrix[p.size()][p.size()];
 
-    return i;
+    for (int i = 0; i < p.size(); ++i) {
+        matrix[0][i] = 0;
+        matrix[i][0] = 0;
+        matrix[i + 1][i + 1] = 0;       // set diagonal cost to 0
+    }
+
+    for (int chainLength = 2; chainLength < p.size(); ++chainLength) {
+        for (int i = 1; i < p.size() - chainLength + 1; ++i) {
+            int j = i + chainLength - 1;
+            matrix[i][j] = INT_MAX;
+            for (int k = i; k < j; ++k) {
+                int mpy = matrix[i][k] + matrix[k + 1][j] + p[i - 1] * p[j] * p[k];
+                if (mpy < matrix[i][j]) {
+                    matrix[i][j] = mpy;
+                    matrix[j][i] = k;
+                }
+            }
+
+        }
+    }
+
+    cout << "Min Cost = " << matrix[1][p.size() - 1];
 }
-vector<vector<int>> calcMinMultiplicationTimes(vector<int> p) {
-    vector<vector<int>> mulTimes;
-    vector<vector<int>> cutLocation;
-    mulTimes.resize(matrixAmount, vector<int>(matrixAmount, 0));
-    cutLocation.resize(matrixAmount, vector<int>(matrixAmount, 0));
 
-
-    return mulTimes;
-}
-
-void calcCutLocation(int i,int j, int k){
-    cutLoc.resize(matrixAmount,vector<int>(matrixAmount,0));
+void calcCutLocation(int i, int j, int k) {
+    cutLoc.resize(matrixAmount, vector<int>(matrixAmount, 0));
     cutLoc[i][j] = k;
 }
 
@@ -34,6 +48,6 @@ int main() {
     }
 
     //vector<vector<int>> getTimes = calcMinMultiplicationTimes(p);
-
+    matrixChainMul(p);
 
 }
